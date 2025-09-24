adfcadcab2c4a6362dfd94cf83fd6f6e308fdf05
unknown <marilupalma52@gmail.com>
2025-09-23 18:38:30 -0600

    Corrigiendo  rutas del Carrusel

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     // ====== CARRUSEL DE IMÁGENES ======
     const imagenes = [
-        "../Imagenes/img1.jpg",
-        "../Imagenes/img2.jpg",
-        "../Imagenes/img3.jpg",
-        "../Imagenes/img4.jpg",
-        "../Imagenes/img5.jpg"
+        "Imagenes/img1.jpg",
+        "Imagenes/img2.jpg",
+        "Imagenes/img3.jpg",
+        "Imagenes/img4.jpg",
+        "Imagenes/img5.jpg"
     ];
 
     let indice = 0;
@@ -21,10 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const globo = document.getElementById("globo");
 
     const frases = [
-        { texto: "¡Hola! Soy tu amigo Agri.", audio: "../audio/agri1.mp3" },
-        { texto: "Recuerda cuidar la naturaleza.", audio: "../audio/agri2.mp3" },
-        { texto: "Comparte conocimiento con tu comunidad.", audio: "../audio/agri3.mp3" },
-        { texto: "La innovación empieza contigo.", audio: "../audio/agri4.mp3" }
+        { texto: "¡Hola! Soy tu amigo Agri.", audio: "audio/agri1.mp3" },
+        { texto: "Recuerda cuidar la naturaleza.", audio: "audio/agri2.mp3" },
+        { texto: "Comparte conocimiento con tu comunidad.", audio: "audio/agri3.mp3" },
+        { texto: "La innovación empieza contigo.", audio: "audio/agri4.mp3" }
     ];
 
     let i = 0;
