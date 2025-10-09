de390f784b292cbb3e776aca09e19722f598237c
unknown <marilupalma52@gmail.com>
2025-10-09 12:17:23 -0600

    Agregando menu en cel

--- JS/menu.js
+++ JS/menu.js
@@ -1,13 +1,10 @@
-// Seleccionamos el botón y el menú
 const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
+const menuList = document.querySelector('#nav-menu ul');
 
-// Evento para abrir/cerrar el menú
 menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+    menuList.classList.toggle('active');
 
-    // Cambiar el ícono del botón de ☰ a X
-    if(navMenu.classList.contains('active')){
+    if(menuList.classList.contains('active')){
         menuToggle.textContent = '✖ Cerrar';
     } else {
         menuToggle.textContent = '☰ Menú';
