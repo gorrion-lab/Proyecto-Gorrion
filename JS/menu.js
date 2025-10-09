91fce0fe01f82b08a06e64347fa4abf4d87641bd
unknown <marilupalma52@gmail.com>
2025-10-09 11:57:31 -0600

    Cambios menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,7 +1,15 @@
+// Seleccionamos el botón y el menú
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
 
+// Evento para abrir/cerrar el menú
 menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-  menuToggle.textContent = navMenu.classList.contains('active') ? '❌' : '☰';
+    navMenu.classList.toggle('active');
+
+    // Cambiar el ícono del botón de ☰ a X
+    if(navMenu.classList.contains('active')){
+        menuToggle.textContent = '✖ Cerrar';
+    } else {
+        menuToggle.textContent = '☰ Menú';
+    }
 });
